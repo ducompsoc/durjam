059e2fc47534e6ef9f952e96356c854d20fba8dd
fn <64966099+f2reninj5@users.noreply.github.com>
2025-03-07 21:09:47 +0000

    Update home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,46 +1,55 @@
-const links = [
-  { name: "Open roles", href: "#" },
-  { name: "Internship program", href: "#" },
-  { name: "Our values", href: "#" },
-  { name: "Meet our leadership", href: "#" },
-]
-const stats = [
-  { name: "Offices worldwide", value: "12" },
-  { name: "Full-time colleagues", value: "300+" },
-  { name: "Hours per week", value: "40" },
-  { name: "Paid time off", value: "Unlimited" },
-]
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Example() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      {/*<img*/}
-      {/*  alt=""*/}
-      {/*  src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"*/}
-      {/*  className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center"*/}
-      {/*/>*/}
+    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32 min-h-screen">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">DurJam</h2>
-          <p className="mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">bla bla bla bla</p>
-          <img src="/durjam_logo_transparent.png" alt="" />
+        <div className="mx-auto max-w-2xl">
+          <span className="flex flex-row gap-5 justify-center my-5">
+            <img
+              src="/durjam_logo_transparent.png"
+              alt="DurJam logo: a cathedral-shaped video game controller."
+              className="w-20"
+            />
+            <h1 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">DurJam</h1>
+          </span>
+          <p className="text-center my-5 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">
+            DU Computing Society's Game Jam & Durham's Society for Game Development!
+          </p>
         </div>
-        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-            {links.map((link) => (
-              <a key={link.name} href={link.href}>
-                {link.name} <span aria-hidden="true">&rarr;</span>
-              </a>
-            ))}
-          </div>
-          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="flex flex-col-reverse gap-1">
-                <dt className="text-base/7 text-gray-300">{stat.name}</dt>
-                <dd className="text-4xl font-semibold tracking-tight text-white">{stat.value}</dd>
-              </div>
-            ))}
-          </dl>
+        <div className="mx-auto max-w-2xl flex flex-row gap-10 justify-center my-5">
+          <a href="https://instagram.com/durjamevent">
+            <FontAwesomeIcon icon={faInstagram} className="text-6xl" />
+          </a>
+          <a href="https://www.linkedin.com/company/durjam">
+            <FontAwesomeIcon icon={faLinkedin} className="text-6xl" />
+          </a>
+          <a href="mailto:oscar.r@durjam.co.uk">
+            <FontAwesomeIcon icon={faEnvelope} className="text-6xl" />
+          </a>
+        </div>
+        <div className="mx-auto mt-5 max-w-2xl flex flex-col">
+          <a
+            href="https://forms.gle/FiR6rnLjmFw562MPA"
+            className="my-5 block text-center mx-auto px-8 py-4 bg-purple-600 text-white rounded-full hover:bg-purple-700"
+          >
+            Register your Interest to Participate!
+          </a>
+          <h2 className="text-3xl font-semibold text-center my-5">Learn more about DurJam</h2>
+          <a
+            href="https://subdued-lupin-877.notion.site/DurJam-JamPacks-15c9eeb1fa3680fea32ecc7c0e8c70c2"
+            className="my-2 block text-center mx-auto px-8 py-4 bg-purple-600 text-white rounded-full hover:bg-purple-700"
+          >
+            Check out JamPacks, our Game Development Tutorials
+          </a>
+          <a
+            href="https://durjam.devpost.com/project-gallery"
+            className="my-2 block text-center mx-auto px-8 py-4 bg-purple-600 text-white rounded-full hover:bg-purple-700"
+          >
+            Check out the Games from DurJam 2020
+          </a>
         </div>
       </div>
     </div>
